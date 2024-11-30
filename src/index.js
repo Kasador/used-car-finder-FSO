8ed32d8d0beeb6f2d864a9194d1ee77617ec224e
Kasador <huntershaw23@gmail.com>
2024-11-30 07:21:55 -0800

    main >>> in-progress <<< switching/making branches for better organization

--- src/index.js
+++ src/index.js
@@ -3,3 +3,35 @@ import './style.scss';
 
 import data from './data.json'
 
+class Main { // Main class
+    constructor() {
+
+    }
+}
+
+class Car { // Main class
+    constructor(year, make, model) {
+        this.year = year;
+        this.make = make;
+        this.model = model;
+    }
+}
+
+class Year extends Car { // Main class
+    constructor(year, make, model) {
+        super(year);
+        this.make = make
+    }
+}
+
+class Make extends Car { // Main class
+    constructor() {
+
+    }
+}
+
+class Model extends Car { // Main class
+    constructor() {
+
+    }
+}

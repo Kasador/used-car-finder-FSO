a69a7aaf17663901268ef38511067ac1756cb4ea
Kasador <huntershaw23@gmail.com>
2024-12-01 06:54:35 -0800

    js >>> update <<< working on pushing data from data.JSON and append data to the front-end / years in-progress

--- src/index.js
+++ src/index.js
@@ -1,25 +1,36 @@
-// Imports your SCSS stylesheet
-// https://stackoverflow.com/questions/52376720/how-to-make-font-awesome-5-work-with-webpack
-// https://stackoverflow.com/questions/37081559/all-my-code-runs-twice-when-compiled-by-webpack
+/* REFERENCES >>>
+    1) font awesome + webpack
+        a) https://stackoverflow.com/questions/52376720/how-to-make-font-awesome-5-work-with-webpack
+            developer notes: // import '@fortawesome/fontawesome-free/js/solid' just found out I need pro :(
+    2) webpack running JS twice 
+        a) https://stackoverflow.com/questions/37081559/all-my-code-runs-twice-when-compiled-by-webpack
+    3) sorting + array methods and functions
+        a) https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+*/
 
 import './style.scss';
 import data from './data.json'
-// import '@fortawesome/fontawesome-free/js/solid' just found out I need pro :(
 
 const carData = new Array(data);
 
 console.log(carData, 'The car data');
+console.log(carData[0].length, 'The car data');
 
 class Main { // Main class
     constructor() {
+        this.yearArray = [];
+        this.makeArray = [];
+        this.moduleArray = [];
+
         this.setInit = false; // start app and use state management
         this.init();
-        this.buildDOM();
     }
 
     init() { // start the application
         this.setInit = true;
         console.log('Application successfully started:', this.setInit);
+        // this.buildDOM();
+        this.getYears();
     }
 
     buildDOM() { // build the DOM elements needed for backend appending data from JSON
@@ -35,33 +46,79 @@ class Main { // Main class
         findSelectElement.append(option);
         // this.findSelectElement.document.append(option);
     }
-}
 
-class Car { // Main class
-    constructor(year, make, model) {
-        this.year = year;
-        this.make = make;
-        this.model = model;
+    getYears() {
+        const storeYears = [];
+
+        carData[0].forEach(items => {
+            storeYears.push(items.year);
+        });
+
+        console.log(storeYears, "the years");
+    }
+
+    getMakes() {
+        
+    }
+
+    getModels() {
+        
     }
 }
 
-class Year extends Car { // Main class
-    constructor(year, make, model) {
+class Year { // Main class
+    constructor(year) {
+        this.year = year;
+    }
+
+    getYear() {
+
+    }
+}
+
+class Make extends Year { // Main class
+    constructor(year, make) {
         super(year);
         this.make = make
     }
-}
 
-class Make extends Car { // Main class
-    constructor() {
-
+    getMake() {
+        console.log("Year");
+        // carData[0].forEach(year => {
+        //     console.log(year, "the years");
+        // })
     }
 }
 
-class Model extends Car { // Main class
-    constructor() {
+class Model extends Make { // Main class
+    constructor(year, make, model) {
+        super(year, make);
+        this.model = model;
+    }
 
+    getModel() {
+        console.log("Made");
+        // carData[0].forEach(year => {
+        //     console.log(year, "the years");
+        // })
+    }
+}
+
+class Car extends Model { // Main class
+    constructor(year, make, model) {
+        super(year, make, model);
+    }
+
+    displaySelectedCar() {
+        console.log(`The year is ${this.year} and the make is ${this.make} and finally the model is ${this.model}.`)
+        // carData[0].forEach(year => {
+        //     console.log(year, "the years");
+        // })
     }
 }
 
 const main = new Main();
+
+const car = new Car(2018, 'BMW', "Model");
+
+console.log(car.displaySelectedCar());
